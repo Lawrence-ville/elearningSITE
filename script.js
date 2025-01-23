6762c37eaae675f51d87ace5fccdaafcf8e5ad14
Lawrence-Alpha <125441838+Lawrence-ville@users.noreply.github.com>
2025-01-23 17:43:33 +0300

    Add files via upload
    my school project

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function AuthenticatorAssertionResponse() {
+            var email=document.getElementById('email');value;n
+            var password=document.getElementById('password');value;
+            if (email=="admin@gmail.com" && password =="12345Alpha")
+            {
+                window.location.assign='students.html';
+                alert("login succesfully");
+            }
+            else{
+                alert ("Invalid Information");
+                return;
+            }            
+}
+
+
+function AuthenticatorAssertionResponse() {
+    var email=document.getElementById('email');value;n
+    var password=document.getElementById('password');value;
+    if (email=="admin@gmail.com" && password =="12345Alpha")
+    {
+        window.location.assign='students.html';
+        alert("login succesfully")
+    }
+    else{
+        alert ("Invalid Information");
+        return;
+    }            
+
+}
+  
